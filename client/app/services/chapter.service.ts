c37ccdba1ca9a700607d8d4f063db33111f12a38
The Rationalizer <TheRationaliser@gmail.com>
2017-09-03 16:23:25 +0100

    Cache chapters

--- client/app/services/chapter.service.ts
+++ client/app/services/chapter.service.ts
@@ -4,16 +4,28 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Api } from '../../../server/api-models/chapter';
 import { Http } from '@angular/http';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 
 @Injectable()
 export class ChapterService {
+  private hasData = false;
+  private chaptersSubject = new BehaviorSubject<Api.Models.Chapter[]>(null);
 
   constructor(private http: Http) { }
 
   public getChapters(): Observable<Api.Models.Chapter[]> {
-    console.log('Getting data');
-    return this.http.get('http://localhost:4200/api/chapters').map(res => res.json());
+    if (!this.hasData) {
+      this.http
+        .get('http://localhost:4200/api/chapters')
+        .map(res => res.json())
+        .subscribe((data: Api.Models.Chapter[]) => {
+          this.hasData = true;
+          this.chaptersSubject.next(data);
+        });
+    }
+    
+    return this.chaptersSubject.asObservable();
   }
 
 }
